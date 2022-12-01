c744c4be5028b20a2413f1243c3313186c5fee09
haofengjiang <115331712+haofengjiang@users.noreply.github.com>
2022-12-01 18:38:41 +0800

    release web3 api

---
+++ cobo_custody/base_test.go
@@ -0,0 +1,52 @@
+package cobo_custody
+
+import (
+	"flag"
+	"testing"
+)
+
+var env = flag.String("env", "Sandbox", "Env Config")
+var secret = flag.String("secret", "Demo", "Api Secrect")
+var web3Secret = flag.String("web3Secret", "1fb9a269028e485f17b0fb05151dfa23ce17a2ac6413163ba7c29b84080b72a2", "Web3 Api Secrect")
+
+var ConfigData Config
+var client Client
+var web3Client Web3Client
+
+func GetEnv(env string) Env {
+	if env == "Prod" {
+		return Prod()
+	}
+	return Sandbox()
+}
+func GetData(env string) Config {
+	if env == "Prod" {
+		return ProdConfig()
+	}
+	return SandboxConfig()
+}
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	var localSigner = LocalSigner{
+		PrivateKey: *secret,
+	}
+	ConfigData = GetData(*env)
+	client = Client{
+		Signer: localSigner,
+		Env:    GetEnv(*env),
+		Debug:  false,
+	}
+
+	var web3LocalSigner = LocalSigner{
+		PrivateKey: *web3Secret,
+	}
+	ConfigData = GetData(*env)
+	web3Client = Web3Client{
+		Signer: web3LocalSigner,
+		Env:    GetEnv(*env),
+		Debug:  false,
+	}
+
+	m.Run()
+}
